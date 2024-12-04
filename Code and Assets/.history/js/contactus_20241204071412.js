72ddb754c70fb12615cb06154e530c09ea9131bb
sugralld <shafaldabiilqis@gmail.com>
2024-12-04 08:51:27 +0700

    Modification added

---
+++ Code and Assets/.history/js/contactus_20241204071412.js
@@ -0,0 +1,72 @@
+function wordLen(str) {
+    const value = str.trim().split(/\s+/);
+    return value.length;
+}
+
+function checkAlpha(password){
+    var isAlpha = false;
+    for(let i=0; i<password.length; i++){
+        if(isNaN(password[i])){
+            isAlpha = true;
+        }
+        else{
+            return false
+        }
+
+        if(isAlpha || isNum){
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkNum(password){
+    var isAlpha = false;
+    var isNum = false;
+    for(let i=0; i<password.length; i++){
+        if(isNaN(password[i])){
+            isAlpha = true;
+        }
+        else{
+            isNum = true;
+        }
+
+        if(isAlpha || isNum){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+function validateData(){
+    var firstname = document.getElementById("firstname");
+    var lastname = document.getElementById("lastname");
+    var email = document.getElementById("email");
+    var phonenum = document.getElementById("phonenum");
+    var message = document.getElementById("message");
+    var agreement = document.getElementById("agreement");
+
+    if(!checkAlphaNum(firstname.value)){
+        alert("First name must be alphabet!");
+    }
+    else if(!checkAlphaNum(lastname.value)){
+        alert("Last name must be alphabet!");
+    }
+    else if(!email.value.endsWith("@gmail.com")){
+        alert("Email must end with '@gmail.com'!");
+    }
+    else if(!checkAlphaNum(phonenum.value)){
+        alert("Phone number must be numeric!");
+    }
+    else if(wordLen(message.value)<5){
+        alert("Message must be at least 5 words!");
+    }
+    else if(!agreement.checked){
+        alert("'Done' checkbox must be checked!");
+    }
+    else{
+        alert("Message Submitted.");
+    }
+
+}
---
+++ Code and Assets/.history/js/contactus_20241204074105.js
@@ -0,0 +1,69 @@
+function wordLen(str) {
+    const value = str.trim().split(/\s+/);
+    return value.length;
+}
+
+function checkAlpha(inputStr){
+    var isAlpha = false;
+    for(let i=0; i<inputStr.length; i++){
+        if(isNaN(inputStr[i])){
+            isAlpha = true;
+        }
+        else if(inputStr[i]!=' '){
+            return false;
+        }
+    }
+    if(isAlpha )return true;
+    else return false;
+}
+
+function checkNum(inputStr){
+    var isNum = false;
+    for(let i=0; i<inputStr.length; i++){
+        if(isNaN(inputStr[i])){
+            return false;
+        }
+        else{
+            isNum = true;
+        }
+    }
+    if(isNum)return true;
+    else return false;
+}
+
+
+function validateData(){
+    var firstname = document.getElementById("firstname");
+    var lastname = document.getElementById("lastname");
+    var email = document.getElementById("email");
+    var phonenum = document.getElementById("phonenum");
+    var message = document.getElementById("message");
+    var agreement = document.getElementById("agreement");
+
+    if(!checkAlpha(firstname.value)){
+        alert("First name must be alphabet!");
+    }
+    else if(!checkAlpha(lastname.value)){
+        alert("Last name must be alphabet!");
+    }
+    else if(!email.value.endsWith("@gmail.com")){
+        alert("Email must end with '@gmail.com'!");
+    }
+    else if(!checkNum(phonenum.value)){
+        alert("Phone number must be numeric!");
+    }
+    else if(wordLen(message.value)<5){
+        alert("Message must be at least 5 words!");
+    }
+    else if(!agreement.checked){
+        alert("'Done' checkbox must be checked!");
+    }
+    else{
+        alert("Message Submitted.");
+    }
+
+}
+
+function resetForm(){
+    document.getElementById("contactForm").re
+}
